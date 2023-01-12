b0758467216a22d928e5d156270d6ac6486bb888
Dave Kobrenski <dave@bbmdesigns.com>
2023-01-12 12:15:59 -0500

    bug fix

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import upath from 'upath';
 import chalk from 'chalk';
 import path from 'path';
 import {fileURLToPath} from 'url';
@@ -8,9 +9,11 @@ import { create } from 'create-create-app';
 // const { resolve } = require('path');
 //const { create } = require('create-create-app');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-let templateRoot = resolve(__dirname, '../templates');
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// let templateRoot = resolve(__dirname, '../templates');
+
+const templateRoot = upath.resolve(__dirname, '../templates')
 
 console.log(chalk.redBright("DEBUG:"));
 console.log(__filename);
@@ -24,12 +27,5 @@ Installed successfully.
 `;
 
 create('create-directus-docker', {
-  templateRoot,
-  after: ({ answers }) => {
-    //custom stuff to do or print out here...
-    console.log(templateRoot);
-    let msg = `cd ${answers.name} && npm start`
-    console.log(`To configure and launch all services, run ${chalk.green(msg)}`);
-  },
-  caveat,
+  templateRoot
 });
