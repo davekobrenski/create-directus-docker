d726974c6c82df9671e797ebce03ebcb88f8a6fe
Dave Kobrenski <dave@bbmdesigns.com>
2023-01-12 12:47:40 -0500

    instructions update

--- src/cli.js
+++ src/cli.js
@@ -7,22 +7,22 @@ import {fileURLToPath} from 'url';
 import { create } from 'create-create-app';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename);)
 
-const caveat = `
-${chalk.yellow("Directus with MySQL, Adminer, and GraphiQL:")}
+create('create-directus-docker', {
+  templateRoot: upath.resolve(__dirname, '../templates'),
+  caveat: ({ answers }) => {
+    return `
+{chalk.yellow("Directus with MySQL, Adminer, and GraphiQL:")}
 
 To configure and run your Directus services, run:
 
-cd ${chalk.bold.green(options.name)} && npm start
+cd ${chalk.bold.green(answers.name)} && npm start
 
 For more information, see: 
 https://github.com/davekobrenski/create-directus-docker
 
 Enjoy!
-`;
-
-create('create-directus-docker', {
-  templateRoot: upath.resolve(__dirname, '../templates'),
-  caveat
+`
+  }
 });
