64ec531649dfd13751254f5154024c3869af4f16
Dave Kobrenski <dave@bbmdesigns.com>
2023-01-12 12:11:20 -0500

    searching for fix...

--- src/cli.js
+++ src/cli.js
@@ -12,19 +12,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 let templateRoot = resolve(__dirname, '../templates');
 
+console.log(chalk.redBright("DEBUG:"));
+console.log(__filename);
+console.log(__dirname)
 console.log(templateRoot);
+console.log(chalk.redBright("END DEBUG"));
+//${chalk.yellow("Directus with MySQL, Adminer, and GraphiQL:")}
 
 const caveat = `
-${chalk.yellow("Directus with MySQL, Adminer, and GraphiQL:")}
-
 Installed successfully.
-
 `;
 
 create('create-directus-docker', {
   templateRoot,
   after: ({ answers }) => {
     //custom stuff to do or print out here...
+    console.log(templateRoot);
     let msg = `cd ${answers.name} && npm start`
     console.log(`To configure and launch all services, run ${chalk.green(msg)}`);
   },
