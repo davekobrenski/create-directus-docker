d93f1eca576ea16e6d31e78a2f31f7f0016e401c
Dave Kobrenski <dave@bbmdesigns.com>
2023-01-12 18:02:02 -0500

    added install to cmds

--- src/cli.js
+++ src/cli.js
@@ -5,12 +5,36 @@ import chalk from 'chalk';
 import path from 'path';
 import {fileURLToPath} from 'url';
 import { create } from 'create-create-app';
+import { spawn } from 'node:child_process';
+import logUpdate from 'log-update';
+import {execa} from 'execa';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 create('create-directus-docker', {
   templateRoot: upath.resolve(__dirname, '../templates'),
+  promptForDescription: false,
+  promptForAuthor: false,
+  promptForEmail: false,
+  promptForLicense: false,
+  
+  after: ({packageDir}) => {
+    const cd = spawn('cd', packageDir);
+    cd.on('close', code => {
+      const install = spawn('npm', ['install']);
+
+      install.stdout.on('data', (data) => {
+        logUpdate(`${data}`);
+      });
+
+      install.on('close', (code) => {
+        logUpdate.done();
+        execa('npm', ['start']).stdout.pipe(process.stdout);
+      });
+    });
+  },
+
   caveat: ({ packageDir }) => {
     return `
 ${chalk.bold.yellowBright("Directus with MySQL, Adminer, and GraphiQL:")}
