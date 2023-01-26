68cf3ec1b1f899de684ebcd20257cf6fd347d218
Dave Kobrenski <dave@bbmdesigns.com>
2023-01-26 13:12:57 -0500

    updated installer script, removed deps

--- src/cli.js
+++ src/cli.js
@@ -3,40 +3,26 @@
 import upath from 'upath';
 import chalk from 'chalk';
 import path from 'path';
-import {fileURLToPath} from 'url';
-import { create } from 'create-create-app';
-// import { spawn } from 'node:child_process';
-// import logUpdate from 'log-update';
-// import {execa} from 'execa';
+import { fileURLToPath } from 'url';
+import logUpdate from 'log-update';
+import fs from 'fs-extra';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const templateDir = upath.resolve(__dirname, '../templates/default');
 
-create('create-directus-docker', {
-  templateRoot: upath.resolve(__dirname, '../templates'),
-  promptForDescription: false,
-  promptForAuthor: false,
-  promptForEmail: false,
-  promptForLicense: false,
+const rootPath = path.resolve();
 
-  // after: ({packageDir}) => {
-    // const cd = spawn('cd', [packageDir]);
-    // cd.on('close', code => {
-    //   const install = spawn('npm', ['install']);
+const argv = yargs(hideBin(process.argv)).argv;
+const args = argv._;
 
-    //   install.stdout.on('data', (data) => {
-    //     logUpdate(`${data}`);
-    //   });
+//check enough args
+if (args.length > 0) {
+  const packageDir = args[0];
 
-    //   install.on('close', (code) => {
-    //     logUpdate.done();
-    //     execa('npm', ['start']).stdout.pipe(process.stdout);
-    //   });
-    // });
-  // },
-
-  caveat: ({ packageDir }) => {
-    return `
+  const successMsg = `
 ${chalk.bold.yellowBright("Directus with MySQL, Adminer, and GraphiQL:")}
 
 To configure and run your Directus services, first ${chalk.bold("make sure you have Docker installed and running")}, then run:
@@ -49,6 +35,28 @@ For more information, see:
 https://github.com/davekobrenski/create-directus-docker
 
 Enjoy!
-`
+`;
+  console.log("");
+  logUpdate(`Creating directory: ${packageDir}...`);
+  const dirFullPath = path.join(rootPath, packageDir);
+
+  //make the dir for the project
+  if (fs.existsSync(dirFullPath) === false) {
+    fs.mkdir(path.join(rootPath, packageDir)).then(() => {
+      //success
+      logUpdate(chalk.greenBright(`Directory '${packageDir}' created successfully.`));
+      logUpdate.done();
+
+      try {
+        fs.copySync(templateDir, dirFullPath);
+        logUpdate(successMsg);
+      } catch (err) {
+        console.error(err);
+      } 
+    }).catch(err => console.error(err));
+  } else {
+    console.error(chalk.redBright(`Error: Specified directory '${packageDir}' already exists.`));
   }
-});
+} else {
+  console.error(chalk.redBright(`Error: please specify a project directory.`));
+}
